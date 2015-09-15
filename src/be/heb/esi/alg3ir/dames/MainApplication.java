ca39103bb0d79a0325a78ff262d04fd51c2310e6
Bruno Parmentier <38496@heb.be>
2015-09-15 23:19:56 +0200

    Reorganize project files

---
+++ src/be/heb/esi/alg3ir/dames/MainApplication.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015 Bruno Parmentier - Jonathan Wyckmans
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package be.heb.esi.alg3ir.dames;
+
+/**
+ *
+ * @author G38496
+ */
+public class MainApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
