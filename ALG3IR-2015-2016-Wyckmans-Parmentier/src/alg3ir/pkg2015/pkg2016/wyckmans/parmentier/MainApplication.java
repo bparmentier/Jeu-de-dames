930c946800a6ac54eebedd54a1743f40a4f91579
Bruno Parmentier <38496@heb.be>
2015-09-15 15:33:32 +0200

    Initial commit

---
+++ ALG3IR-2015-2016-Wyckmans-Parmentier/src/alg3ir/pkg2015/pkg2016/wyckmans/parmentier/MainApplication.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alg3ir.pkg2015.pkg2016.wyckmans.parmentier;
+
+/**
+ *
+ * @author G38496
+ */
+public class MainApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
